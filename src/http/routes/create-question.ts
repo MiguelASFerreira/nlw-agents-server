c129f6a24bbf470c0bc6d8a27abcb4efd1010565
MiguelASFerreira <migant1306@gmail.com>
2025-07-10 20:21:19 -0300

    Atualiza a rota de criação de perguntas para incluir geração de respostas com base em transcrições e embeddings

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,7 +1,9 @@
+import { and, eq, sql } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { generateAnswer, generateEmbeddings } from '../../services/gemini.ts'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -9,7 +11,7 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
     {
       schema: {
         body: z.object({
-          question: z.string().min(1)
+          question: z.string().min(1),
         }),
         params: z.object({
           roomId: z.string(),
@@ -20,10 +22,44 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { roomId } = request.params
       const { question } = request.body
 
-      const result = await db.insert(schema.questions).values({
-        roomId,
-        question,
-      }).returning()
+      const embeddings = await generateEmbeddings(question)
+
+      const embeddingsAsString = `[${embeddings.join(',')}]`
+
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${embeddingsAsString}::vector`
+        )
+        .limit(3)
+
+      let answer: string | null = null
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+        answer = await generateAnswer(question, transcriptions)
+      }
+      
+      const result = await db
+        .insert(schema.questions)
+        .values({
+          roomId,
+          question,
+          answer,
+        })
+        .returning()
 
       const insertedQuestion = result[0]
 
